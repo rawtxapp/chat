8826db23c16184da03a3b118ac53007b56e325b8
F <f@rawtx.com>
2019-03-21 17:25:49 -0700

    switch Micro to be typescript.

--- src/Micro.tsx
+++ src/Micro.tsx
@@ -1,33 +1,32 @@
 export default class Micro {
-  constructor() {
-    this.allowance = 0;
-  }
+  allowance : number = 0;
+  initialized : boolean = false;
 
   init() {
-    document.addEventListener("message", ({ data: rawData }) => {
+    document.addEventListener("message", ({ data: rawData }:any) => {
       if (!rawData) return;
       let data = rawData.toLowerCase();
       const appAllowancePrefix = "appallowance:";
       if (data == "initmicro") {
         this.initialized = true;
-        window.postMessage("initmicroack");
+        window.postMessage("initmicroack", "*");
       } else if (data.startsWith(appAllowancePrefix)) {
         let newAllowance = data.substring(appAllowancePrefix.length);
         this.allowance = parseInt(newAllowance);
       }
     });
-  }
+  };
 
-  canHandleWithMicro = amount => {
+  canHandleWithMicro = (amount: number) => {
     if (this.initialized && this.allowance > amount) {
       return true;
     }
     return false;
   };
 
-  handleWithMicro = (invoice, amount) => {
+  handleWithMicro = (invoice: string, amount: number) => {
     if (this.canHandleWithMicro(amount)) {
-      window.postMessage("lightning:" + invoice);
+      window.postMessage("lightning:" + invoice, "*");
       return true;
     }
     return false;
