d08309b7ad7991a0127031ca2d0d1adcc2a5a389
F <f@rawtx.com>
2019-03-24 21:03:44 -0700

    change ChatBackend to typescript.

--- src/ChatBackend.ts
+++ src/ChatBackend.ts
@@ -1,4 +1,5 @@
 export default class ChatBackend {
+  endpoint: string;
   constructor() {
     if (
       process.env.NODE_ENV &&
@@ -15,7 +16,7 @@ export default class ChatBackend {
     return this.endpoint + "/" + path;
   };
 
-  getInvoice = async memo => {
+  getInvoice = async (memo:string) => {
     const response = await fetch(this.url("invoice/" + memo));
     return response.json();
   };
